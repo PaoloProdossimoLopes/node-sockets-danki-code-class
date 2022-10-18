9ddabb3ecdce37692b4b8a39c279cb4c5d5c9373
Paolo Prodossimo Lopes <70642432+PaoloProdossimoLopes@users.noreply.github.com>
2022-10-18 05:26:09 -0300

    Feature (Chat): Done conversation process

--- application/index.js
+++ application/index.js
@@ -2,17 +2,45 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+let usuarios = [];
+let socketIds = [];
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection',(socket)=>{
-    socket.broadcast.emit('novo usuario','Um novo usuário se conectou!');
-    socket.on('disconnect',()=>{
+io.on('connection', (socket) => {
+    socket.broadcast.emit('novo usuario','Um novo usuário se conectou!'); //Notifica apenas outros sockets (usuarios conectados)
+
+    socket.on('new-user', (data) => {
+        if (usuarios.indexOf(data) != -1) {
+            socket.emit('new-user', { success: false });
+        } else {
+            usuarios.push(data);
+            socketIds.push(socket.id);
+
+            socket.emit('new-user', { sucess: true });
+        }
+    });
+
+    socket.on('disconnect', () => { //Notifica quando o usuario se desconectou
+        let id = sockeIds.indexOf(socket.id);
+        socketIds.splice(id, 1);
+        usuarios.splice(id, 1);
+
         console.log('Desconectado.');
-    })
+    });
+
+    socket.on('chat-message', (model) => {
+        if (usuarios.indexOf(model.name) != -1 && usuarios.indexOf(model.nome) == socketIds.indexOf(socket.id)) {
+            io.emit('chat-message', model);
+        } else {
+            console.log('Error: Voce nao tem permissao para executar essa açao!')
+        }
+    });
 })
 
-http.listen(3000, () => {
-  console.log('listening on *:3000');
+const port = 3000
+http.listen(port, () => {
+  console.log('listening on localhost:3000');
 });
